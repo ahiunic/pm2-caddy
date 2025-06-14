8d01bbb28d3d344d4ad6bec8063288be23630836
hiteshchoudhary <hitesh@hiteshchoudhary.com>
2025-06-14 20:59:30 +0530

    test

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/crash", (req, res) => {
+  res.send("App is crashing...");
+  process.exit(1);
+});
+
+// take input from param and return it back
+app.get("/:id", (req, res) => {
+  res.send(`hello world ${req.params.id}`);
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
